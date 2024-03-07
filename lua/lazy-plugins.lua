088b8fedf27a59b426161d7dddda2a1294249ceb
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-03-07 09:54:54 +0100

    modified lazy-plugins to use all the plugins in kickstart dir

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -27,7 +27,8 @@ require('lazy').setup({
 
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
-
+  { import = 'kickstart.plugins' },
+  --[[
   require 'kickstart/plugins/gitsigns',
 
   require 'kickstart/plugins/which-key',
@@ -40,14 +41,14 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
---  require 'kickstart/plugins/tokyonight',
+  require 'kickstart/plugins/tokyonight',
 
   require 'kickstart/plugins/todo-comments',
 
   require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
-
+  --]]
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
