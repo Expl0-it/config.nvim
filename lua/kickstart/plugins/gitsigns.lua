ecbaa1e9cf1aa1d578b4c36785fde3942859fc45
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-08-05 20:55:58 +0200

    turned gitblame on in gitsigns.lua

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -64,6 +64,9 @@ return {
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
         map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
       end,
+
+      -- Options
+      current_line_blame = true,
     },
   },
 }
