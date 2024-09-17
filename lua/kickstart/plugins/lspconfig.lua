b9aacc273b525a7a08202dbe2319e2a86da8832f
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-09-17 18:49:21 +0200

    removed redundant statement (K hover working previously)

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -110,9 +110,6 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-          -- Displays hover information about the symbol under the cursor in a floating window.
-          map('K', vim.lsp.buf.hover, '[K] Hover Documentation')
-
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
